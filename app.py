93f1f76e8a94ed1815a2afe6cede24112eb5e9c1
Salih Alper Çetin <145145933+salihalperc@users.noreply.github.com>
2025-07-16 00:02:45 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, jsonify
+import smtplib
+import os
+from dotenv import load_dotenv
+from flask_cors import CORS
+load_dotenv()  # .env dosyasından çevresel değişkenleri yükle
+
+app = Flask(__name__)
+CORS(app)
+
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 587
+EMAIL_ADDRESS = os.getenv('EMAIL_USER')
+EMAIL_PASSWORD = os.getenv('EMAIL_PASS')
+
+
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+@app.route('/send-mail', methods=['POST'])
+def send_mail():
+    data = request.json
+    message = data.get('message')
+
+    subject = "Özür Mesajı"
+    body = f"{message}"
+
+    # UTF-8 destekli MIME mesajı oluştur
+    msg = MIMEMultipart()
+    msg['From'] = EMAIL_ADDRESS
+    msg['To'] = EMAIL_ADDRESS
+    msg['Subject'] = subject
+
+    msg.attach(MIMEText(body, 'plain', 'utf-8'))
+
+    try:
+        with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
+            server.starttls()
+            server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+            server.send_message(msg)
+        return jsonify({"status": "success", "message": "Mail gönderildi"})
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
